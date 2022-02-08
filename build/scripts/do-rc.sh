1617bb3c003d54a90d52cd94ed52dec6a00465de
cpu1 <patwal.chetan@gmail.com>
2022-02-08 15:51:41 +0530

    Test

---
+++ build/scripts/do-rc.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -ex
+
+if [ -z "${GITHUB_REF_NAME}" ] || [ "${GITHUB_REF_TYPE}" != "tag" ] ; then
+    echo "Expected a tag push event, skipping release workflow";
+    exit 1;
+fi
+
+RELEASE_NOTES_FILE="docs/release_notes/${GITHUB_REF_NAME/-rc.*}.md"
+
+if [[ ! -f "${RELEASE_NOTES_FILE}" ]]; then
+    echo "Release notes file ${RELEASE_NOTES_FILE} does not exist. Exiting..."
+    exit 1
+fi
+
+export RELEASE_DESCRIPTION="${GITHUB_REF_NAME}"
+
+make build-all
+#goreleaser release --rm-dist --timeout 60m --skip-validate --config=./.goreleaser.yml --release-notes="${RELEASE_NOTES_FILE}"
